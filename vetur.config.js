3730de70f5dee2b511aa03830f232c99c2f72ec8
Abul Kalam Faruk <abulkalamfaruk@gmail.com>
2021-09-04 13:22:44 +0600

    start again

--- vetur.config.js
+++ vetur.config.js
@@ -11,21 +11,21 @@ module.exports = {
     // **optional** default: `[{ root: './' }]`
     // support monorepos
     projects: [ 
-      './web-app',  // Shorthand for specifying only the project root location
+      './frontend',  // Shorthand for specifying only the project root location
       {
         // **required**
         // Where is your project?
         // It is relative to `vetur.config.js`.
-        root: './web-app',
+        root: './frontend',
         // **optional** default: `'package.json'`
         // Where is `package.json` in the project?
         // We use it to determine the version of vue.
         // It is relative to root property.
-        package: './web-app/package.json',
+        package: './frontend/package.json',
         // **optional**
         // Where is TypeScript config file in the project?
         // It is relative to root property.
-        tsconfig: './web-app/tsconfig.json',
+        tsconfig: './frontend/tsconfig.json',
         // **optional** default: `'./.vscode/vetur/snippets'`
         // Where is vetur custom snippets folders?
         snippetFolder: './.vscode/vetur/snippets',
@@ -35,7 +35,7 @@ module.exports = {
         // It is relative to root property.
         // Notice: It won't actually do it. You need to use `require.context` or `Vue.component`
         globalComponents: [
-          './web-app/src/components/**/*.vue'
+          './frontend/src/components/**/*.vue'
         ]
       }
     ]
